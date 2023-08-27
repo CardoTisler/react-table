90ea7ef606b694d457a92c4afb8de859824fa9a9
CardoTisler <cardotisler@proton.me>
2023-08-28 02:15:04 +0300

    Remove leftover comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import './App.css';
 import Table from './components/Table';
-// Typescript
-// React
-// material-ui@latest
-// react-final-form@latest
-// react-table@7.*.*
-// TODO: Consider Row-level state management for "cancel" functionality to revert back to default values.
+
 function App() {
   return (
     <div className="App">
